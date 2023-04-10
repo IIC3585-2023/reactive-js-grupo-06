1d0365dc7e333aeac9de73188d0bfd980523be72
miguel <miortiz1@uc.cl>
2023-04-09 22:08:01 -0400

    movimiento y colisiones

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,3 +1,8 @@
+const { fromEvent } = rxjs;
+const { map, debounceTime, mapTo, filter} = rxjs.operators;
+const { merge, interval}= rxjs
+
+
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
@@ -6,16 +11,17 @@ canvas.height = innerHeight;
 class Wall {
   static width = 25;
   static height = 25;
-  constructor({ position, image }) {
+  constructor({ position, image, type }) {
     this.position = position;
-    this.width = 25;
+    this.width = 25; 
     this.height = 25;
     this.image = image;
-  }
+    this.type = type;
+  } 
 
   draw() {
-    console.log("draw wall");
-    console.log(this.position.x, this.position.y);
+    // console.log("draw wall");
+    // console.log(this.position.x, this.position.y);
 
     const img = new Image();
     img.src = this.image;
@@ -96,10 +102,99 @@ gameMap.forEach((row, i) => {
       new Wall({
         position: { x: Wall.width * j, y: Wall.height * i },
         image: images[cell],
+        type: cell
       })
     );
   });
 });
 
-console.log(walls.length);
+
+
+
+
+
+
+const canvas2 = document.getElementById("game-players");
+const c = canvas2.getContext('2d');
+canvas2.width = innerWidth;
+canvas2.height = innerHeight;
+
+class Packman {
+  constructor({position, velocity}) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 6;
+  } 
+
+  draw() {
+    c.beginPath()
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+    c.fillStyle = 'white'
+    c.fill()
+    c.closePath()
+  }
+  //  checkColision() {
+  //   const nextPosition = {
+  //     x: ((this.position.x + this.velocity.x)+(gameMap[0].length * Wall.width))%(gameMap[0].length * Wall.width),
+  //     y:((this.position.y + this.velocity.y)+(gameMap.length * Wall.height))%(gameMap.length * Wall.height)
+  //   }
+  //   if (
+  //     Math.trunc((nextPosition.x + this.radius)/Wall.width)
+  //   )
+  //  }
+  move(){
+    this.draw()
+    const nextPosition = {
+      x:((this.position.x + this.velocity.x)+(gameMap[0].length * Wall.width))%(gameMap[0].length * Wall.width),
+      y:((this.position.y + this.velocity.y)+(gameMap.length * Wall.height))%(gameMap.length * Wall.height)
+    }
+    const EsquinaAbajoIzq = gameMap[Math.trunc((nextPosition.y  + this.radius)/Wall.height)][Math.trunc((nextPosition.x  - this.radius)/Wall.width)]
+    const EsquinaArribaDer = gameMap[Math.trunc((nextPosition.y  - this.radius)/Wall.height)][Math.trunc((nextPosition.x  + this.radius)/Wall.width)]
+    const EsquinaAbajoDer = gameMap[Math.trunc((nextPosition.y + this.radius)/Wall.height)][Math.trunc((nextPosition.x + this.radius)/Wall.width)]
+    const EsquinaArribaIzq = gameMap[Math.trunc((nextPosition.y - this.radius)/Wall.height)][Math.trunc((nextPosition.x - this.radius)/Wall.width)]
+    if ((EsquinaAbajoIzq!='.' && EsquinaAbajoIzq!=' ')||(EsquinaArribaDer!='.' && EsquinaArribaDer!=' ')||(EsquinaAbajoDer!='.' && EsquinaAbajoDer!=' ')||(EsquinaArribaIzq!='.' && EsquinaArribaIzq!=' ')) {
+      this.velocity = {x:0, y:0}
+    }
+    this.position.x = ((this.position.x + this.velocity.x)+(gameMap[0].length * Wall.height))%(gameMap[0].length * Wall.height)
+    this.position.y = ((this.position.y + this.velocity.y)+(gameMap.length * Wall.height))%(gameMap.length * Wall.height)
+    c.beginPath()
+    c.rect(Math.trunc((nextPosition.x)/25)*25,Math.trunc((nextPosition.y )/25)*25,25,25)
+    c.strokeStyle = 'grey'
+    c.stroke()
+    c.closePath()
+    // console.log(this.position)
+  }
+}
+
+const player = new Packman({position: {x: 37.5, y: 37.5}, velocity: {x: 0, y: 0}})
+
+
+const leftArrow$ = fromEvent(document, 'keydown').pipe(
+  filter(event => event.key === 'ArrowLeft'),
+  mapTo({ x: -7, y: 0 })
+);
+const rightArrow$ = fromEvent(document, 'keydown').pipe(
+  filter(event => event.key === 'ArrowRight'),
+  mapTo({ x: 7, y: 0 })
+);
+const upArrow$ = fromEvent(document, 'keydown').pipe(
+  filter(event => event.key === 'ArrowUp'),
+  mapTo({ x: 0, y: -7 })
+);
+const downArrow$ = fromEvent(document, 'keydown').pipe(
+  filter(event => event.key === 'ArrowDown'),
+  mapTo({ x: 0, y: 7 })
+);
+
+
+const movepackmanP1$ = merge(leftArrow$, rightArrow$, upArrow$, downArrow$);
+
+movepackmanP1$.subscribe(distance => {
+  player.velocity = distance;
+});
+ const cicle = interval(50);
+
+ cicle.subscribe(() => {c.clearRect(0,0,canvas2.width, canvas2.height);player.move()});
+
 walls.forEach((wall) => wall.draw());
+player.draw()
--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,65 @@
 const { fromEvent } = rxjs;
-const { map, debounceTime } = rxjs.operators;
+const { map, debounceTime, mapTo, filter} = rxjs.operators;
+const { merge, interval}= rxjs
 
 const inputField = document.getElementById("input-field");
 const displayText = document.getElementById("display-text");
 
-const inputObservable = fromEvent(inputField, "input").pipe(
-  map((event) => event.target.value),
-  debounceTime(300)
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+class Packman {
+  constructor({position, velocity}) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 20;
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.position.x + this.radius, this.position.y + this.radius, this.radius, 0, Math.PI * 2)
+    c.fillStyle = 'white'
+    c.fill()
+    c.closePath()
+  }
+
+  move(){
+    this.draw()
+    this.position.x = (this.position.x + this.velocity.x)%300
+    this.position.y = (this.position.y + this.velocity.y)%300
+    console.log(this.position)
+  }
+}
+
+const player = new Packman({position: {x: 0, y: 0}, velocity: {x: 0, y: 0}})
+
+
+const leftArrow$ = fromEvent(document, 'keydown').pipe(
+  filter(event => event.key === 'ArrowLeft'),
+  mapTo({ x: -10, y: 0 })
+);
+const rightArrow$ = fromEvent(document, 'keydown').pipe(
+  filter(event => event.key === 'ArrowRight'),
+  mapTo({ x: 10, y: 0 })
+);
+const upArrow$ = fromEvent(document, 'keydown').pipe(
+  filter(event => event.key === 'ArrowUp'),
+  mapTo({ x: 0, y: -10 })
+);
+const downArrow$ = fromEvent(document, 'keydown').pipe(
+  filter(event => event.key === 'ArrowDown'),
+  mapTo({ x: 0, y: 10 })
 );
 
-inputObservable.subscribe((text) => {
-  displayText.textContent = text;
+
+const movepackmanP1$ = merge(leftArrow$, rightArrow$, upArrow$, downArrow$);
+
+movepackmanP1$.subscribe(distance => {
+  player.velocity = distance;
 });
+ const cicle = interval(50);
+
+ cicle.subscribe(() => {c.clearRect(0,0,canvas.width, canvas.height);player.move()});
+
+
+player.draw()
