4edabd0dde59cef842d60a78e53f6ca97f4e26db
Agustin <aiherrera1@uc.cl>
2023-04-14 15:13:23 -0400

    Pacman cambia la pelota de color

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,23 +1,48 @@
 const { fromEvent } = rxjs;
-const { map, debounceTime, mapTo, filter} = rxjs.operators;
-const { merge, interval}= rxjs
-
+const { map, debounceTime, mapTo, filter } = rxjs.operators;
+const { merge, interval } = rxjs;
 
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+class Pellet {
+  constructor({ position }) {
+    this.position = position;
+    this.radius = 3;
+    this.color = "white";
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(
+      this.position.x + this.radius,
+      this.position.y + this.radius,
+      this.radius,
+      0,
+      Math.PI * 2
+    );
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+  }
+}
+
 class Wall {
   static width = 25;
   static height = 25;
   constructor({ position, image, type }) {
     this.position = position;
-    this.width = 25; 
+    this.width = 25;
     this.height = 25;
     this.image = image;
     this.type = type;
-  } 
+  }
 
   draw() {
     // console.log("draw wall");
@@ -47,25 +72,378 @@ class Wall {
   }
 }
 
+const pellets = [];
 const walls = [];
 
 const gameMap = [
-  [ "1", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "2", ],
-  [ "r", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "l", ],
-  [ "r", ".", "a", "b", ".", "e", "i", "i", "i", "f", ".", "e", "i", "i", "i", "f", ".", "a", "b", ".", "l", ],
-  [ "r", ".", "c", "d", ".", ".", ".", ".", ".", ".", ".", " ", ".", ".", ".", ".", ".", "c", "d", ".", "l", ],
-  [ "r", ".", ".", ".", ".", "a", "b", ".", "a", "-", "-", "-", "b", ".", "a", "b", ".", ".", ".", ".", "l", ],
-  [ "r", ".", "a", "b", ".", "c", "d", ".", "c", "_", "_", "_", "d", ".", "c", "d", ".", "a", "b", ".", "l", ],
-  [ "r", ".", "l", "r", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "l", "r", ".", "l", ],
-  [ "r", ".", "l", "r", ".", "a", "b", ".", "g", " ", " ", " ", "g", ".", "a", "b", ".", "l", "r", ".", "l", ],
-  [ "r", ".", "c", "d", ".", "l", "r", ".", "j", " ", " ", " ", "j", ".", "l", "r", ".", "c", "d", ".", "l", ],
-  [ "r", ".", ".", ".", ".", "l", "r", ".", "l", "-", "-", "-", "r", ".", "l", "r", ".", ".", ".", ".", "l", ],
-  [ "r", ".", "a", "b", ".", "c", "d", ".", "c", "_", "_", "_", "d", ".", "c", "d", ".", "a", "b", ".", "l", ],
-  [ "r", ".", "l", "r", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "l", "r", ".", "l", ],
-  [ "r", ".", "l", "3", "-", "-", "-", "b", ".", "a", "-", "b", ".", "a", "-", "-", "-", "4", "r", ".", "l", ],
-  [ "r", ".", "c", "_", "_", "_", "_", "d", ".", "c", "_", "d", ".", "c", "_", "_", "_", "_", "d", ".", "l", ],
-  [ "r", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "l", ],
-  [ "3", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "4", ],
+  [
+    "1",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "2",
+  ],
+  [
+    "r",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    "a",
+    "b",
+    ".",
+    "e",
+    "i",
+    "i",
+    "i",
+    "f",
+    ".",
+    "e",
+    "i",
+    "i",
+    "i",
+    "f",
+    ".",
+    "a",
+    "b",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    "c",
+    "d",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    " ",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    "c",
+    "d",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    ".",
+    ".",
+    ".",
+    "a",
+    "b",
+    ".",
+    "a",
+    "-",
+    "-",
+    "-",
+    "b",
+    ".",
+    "a",
+    "b",
+    ".",
+    ".",
+    ".",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    "a",
+    "b",
+    ".",
+    "c",
+    "d",
+    ".",
+    "c",
+    "_",
+    "_",
+    "_",
+    "d",
+    ".",
+    "c",
+    "d",
+    ".",
+    "a",
+    "b",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    "l",
+    "r",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    "l",
+    "r",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    "l",
+    "r",
+    ".",
+    "a",
+    "b",
+    ".",
+    "g",
+    " ",
+    " ",
+    " ",
+    "g",
+    ".",
+    "a",
+    "b",
+    ".",
+    "l",
+    "r",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    "c",
+    "d",
+    ".",
+    "l",
+    "r",
+    ".",
+    "j",
+    " ",
+    " ",
+    " ",
+    "j",
+    ".",
+    "l",
+    "r",
+    ".",
+    "c",
+    "d",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    ".",
+    ".",
+    ".",
+    "l",
+    "r",
+    ".",
+    "l",
+    "-",
+    "-",
+    "-",
+    "r",
+    ".",
+    "l",
+    "r",
+    ".",
+    ".",
+    ".",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    "a",
+    "b",
+    ".",
+    "c",
+    "d",
+    ".",
+    "c",
+    "_",
+    "_",
+    "_",
+    "d",
+    ".",
+    "c",
+    "d",
+    ".",
+    "a",
+    "b",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    "l",
+    "r",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    "l",
+    "r",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    "l",
+    "3",
+    "-",
+    "-",
+    "-",
+    "b",
+    ".",
+    "a",
+    "-",
+    "b",
+    ".",
+    "a",
+    "-",
+    "-",
+    "-",
+    "4",
+    "r",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    "c",
+    "_",
+    "_",
+    "_",
+    "_",
+    "d",
+    ".",
+    "c",
+    "_",
+    "d",
+    ".",
+    "c",
+    "_",
+    "_",
+    "_",
+    "_",
+    "d",
+    ".",
+    "l",
+  ],
+  [
+    "r",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    ".",
+    "l",
+  ],
+  [
+    "3",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "-",
+    "4",
+  ],
 ];
 
 function loadImages(src) {
@@ -96,42 +474,50 @@ const images = {
   ".": "img/path.png",
   " ": "img/path.png",
 };
+
 gameMap.forEach((row, i) => {
   row.forEach((cell, j) => {
-    walls.push(
-      new Wall({
-        position: { x: Wall.width * j, y: Wall.height * i },
-        image: images[cell],
-        type: cell
-      })
-    );
+    if (cell === ".") {
+      console.log("pellet");
+      pellets.push(
+        new Pellet({
+          position: {
+            x: Wall.width * j + Wall.width / 2,
+            y: Wall.height * i + Wall.height / 2,
+          },
+        })
+      );
+    } else {
+      walls.push(
+        new Wall({
+          position: { x: Wall.width * j, y: Wall.height * i },
+          image: images[cell],
+          type: cell,
+        })
+      );
+    }
   });
 });
 
-
-
-
-
-
-
 const canvas2 = document.getElementById("game-players");
-const c = canvas2.getContext('2d');
+const c = canvas2.getContext("2d");
 canvas2.width = innerWidth;
 canvas2.height = innerHeight;
 
 class Packman {
-  constructor({position, velocity}) {
+  constructor({ position, velocity, color }) {
     this.position = position;
     this.velocity = velocity;
-    this.radius = 6;
-  } 
+    this.color = color;
+    this.radius = 9;
+  }
 
   draw() {
-    c.beginPath()
-    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
-    c.fillStyle = 'white'
-    c.fill()
-    c.closePath()
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
   }
   //  checkColision() {
   //   const nextPosition = {
@@ -142,59 +528,146 @@ class Packman {
   //     Math.trunc((nextPosition.x + this.radius)/Wall.width)
   //   )
   //  }
-  move(){
-    this.draw()
+  move() {
+    this.draw();
     const nextPosition = {
-      x:((this.position.x + this.velocity.x)+(gameMap[0].length * Wall.width))%(gameMap[0].length * Wall.width),
-      y:((this.position.y + this.velocity.y)+(gameMap.length * Wall.height))%(gameMap.length * Wall.height)
+      x:
+        (this.position.x + this.velocity.x + gameMap[0].length * Wall.width) %
+        (gameMap[0].length * Wall.width),
+      y:
+        (this.position.y + this.velocity.y + gameMap.length * Wall.height) %
+        (gameMap.length * Wall.height),
+    };
+    const EsquinaAbajoIzq =
+      gameMap[Math.trunc((nextPosition.y + this.radius) / Wall.height)][
+        Math.trunc((nextPosition.x - this.radius) / Wall.width)
+      ];
+    const EsquinaArribaDer =
+      gameMap[Math.trunc((nextPosition.y - this.radius) / Wall.height)][
+        Math.trunc((nextPosition.x + this.radius) / Wall.width)
+      ];
+    const EsquinaAbajoDer =
+      gameMap[Math.trunc((nextPosition.y + this.radius) / Wall.height)][
+        Math.trunc((nextPosition.x + this.radius) / Wall.width)
+      ];
+    const EsquinaArribaIzq =
+      gameMap[Math.trunc((nextPosition.y - this.radius) / Wall.height)][
+        Math.trunc((nextPosition.x - this.radius) / Wall.width)
+      ];
+    if (
+      (EsquinaAbajoIzq != "." && EsquinaAbajoIzq != " ") ||
+      (EsquinaArribaDer != "." && EsquinaArribaDer != " ") ||
+      (EsquinaAbajoDer != "." && EsquinaAbajoDer != " ") ||
+      (EsquinaArribaIzq != "." && EsquinaArribaIzq != " ")
+    ) {
+      this.velocity = { x: 0, y: 0 };
     }
-    const EsquinaAbajoIzq = gameMap[Math.trunc((nextPosition.y  + this.radius)/Wall.height)][Math.trunc((nextPosition.x  - this.radius)/Wall.width)]
-    const EsquinaArribaDer = gameMap[Math.trunc((nextPosition.y  - this.radius)/Wall.height)][Math.trunc((nextPosition.x  + this.radius)/Wall.width)]
-    const EsquinaAbajoDer = gameMap[Math.trunc((nextPosition.y + this.radius)/Wall.height)][Math.trunc((nextPosition.x + this.radius)/Wall.width)]
-    const EsquinaArribaIzq = gameMap[Math.trunc((nextPosition.y - this.radius)/Wall.height)][Math.trunc((nextPosition.x - this.radius)/Wall.width)]
-    if ((EsquinaAbajoIzq!='.' && EsquinaAbajoIzq!=' ')||(EsquinaArribaDer!='.' && EsquinaArribaDer!=' ')||(EsquinaAbajoDer!='.' && EsquinaAbajoDer!=' ')||(EsquinaArribaIzq!='.' && EsquinaArribaIzq!=' ')) {
-      this.velocity = {x:0, y:0}
-    }
-    this.position.x = ((this.position.x + this.velocity.x)+(gameMap[0].length * Wall.height))%(gameMap[0].length * Wall.height)
-    this.position.y = ((this.position.y + this.velocity.y)+(gameMap.length * Wall.height))%(gameMap.length * Wall.height)
-    c.beginPath()
-    c.rect(Math.trunc((nextPosition.x)/25)*25,Math.trunc((nextPosition.y )/25)*25,25,25)
-    c.strokeStyle = 'grey'
-    c.stroke()
-    c.closePath()
+    this.position.x =
+      (this.position.x + this.velocity.x + gameMap[0].length * Wall.height) %
+      (gameMap[0].length * Wall.height);
+    this.position.y =
+      (this.position.y + this.velocity.y + gameMap.length * Wall.height) %
+      (gameMap.length * Wall.height);
+    c.beginPath();
+    c.rect(
+      Math.trunc(nextPosition.x / 25) * 25,
+      Math.trunc(nextPosition.y / 25) * 25,
+      25,
+      25
+    );
+    c.strokeStyle = "grey";
+    c.stroke();
+    c.closePath();
     // console.log(this.position)
   }
 }
 
-const player = new Packman({position: {x: 37.5, y: 37.5}, velocity: {x: 0, y: 0}})
+const player1 = new Packman({
+  position: { x: 37.5, y: 37.5 },
+  velocity: { x: 0, y: 0 },
+  color: "blue",
+});
 
+const player2 = new Packman({
+  position: { x: 50, y: 37.5 },
+  velocity: { x: 0, y: 0 },
+  color: "red",
+});
 
-const leftArrow$ = fromEvent(document, 'keydown').pipe(
-  filter(event => event.key === 'ArrowLeft'),
+const leftArrow$ = fromEvent(document, "keydown").pipe(
+  filter((event) => event.key === "ArrowLeft"),
   mapTo({ x: -7, y: 0 })
 );
-const rightArrow$ = fromEvent(document, 'keydown').pipe(
-  filter(event => event.key === 'ArrowRight'),
+const rightArrow$ = fromEvent(document, "keydown").pipe(
+  filter((event) => event.key === "ArrowRight"),
   mapTo({ x: 7, y: 0 })
 );
-const upArrow$ = fromEvent(document, 'keydown').pipe(
-  filter(event => event.key === 'ArrowUp'),
+const upArrow$ = fromEvent(document, "keydown").pipe(
+  filter((event) => event.key === "ArrowUp"),
   mapTo({ x: 0, y: -7 })
 );
-const downArrow$ = fromEvent(document, 'keydown').pipe(
-  filter(event => event.key === 'ArrowDown'),
+const downArrow$ = fromEvent(document, "keydown").pipe(
+  filter((event) => event.key === "ArrowDown"),
   mapTo({ x: 0, y: 7 })
 );
 
-
 const movepackmanP1$ = merge(leftArrow$, rightArrow$, upArrow$, downArrow$);
 
-movepackmanP1$.subscribe(distance => {
-  player.velocity = distance;
+movepackmanP1$.subscribe((distance) => {
+  player1.velocity = distance;
 });
- const cicle = interval(50);
 
- cicle.subscribe(() => {c.clearRect(0,0,canvas2.width, canvas2.height);player.move()});
+const aKey$ = fromEvent(document, "keydown").pipe(
+  filter((event) => event.key === "a"),
+  mapTo({ x: -7, y: 0 })
+);
+const dKey$ = fromEvent(document, "keydown").pipe(
+  filter((event) => event.key === "d"),
+  mapTo({ x: 7, y: 0 })
+);
+const wKey$ = fromEvent(document, "keydown").pipe(
+  filter((event) => event.key === "w"),
+  mapTo({ x: 0, y: -7 })
+);
+const sKey$ = fromEvent(document, "keydown").pipe(
+  filter((event) => event.key === "s"),
+  mapTo({ x: 0, y: 7 })
+);
 
-walls.forEach((wall) => wall.draw());
-player.draw()
+const movepackmanP2$ = merge(aKey$, dKey$, wKey$, sKey$);
+
+movepackmanP2$.subscribe((distance) => {
+  player2.velocity = distance;
+});
+
+const cicle = interval(50);
+
+cicle.subscribe(() => {
+  c.clearRect(0, 0, canvas2.width, canvas2.height);
+  player1.move();
+  player2.move();
+  walls.forEach((wall) => wall.draw());
+  pellets.forEach((pellet) => {
+    pellet.draw();
+    if (
+      Math.hypot(
+        pellet.position.x - player1.position.x,
+        pellet.position.y - player1.position.y
+      ) <
+      pellet.radius + player1.radius
+    ) {
+      pellet.color = "blue";
+    }
+    if (
+      Math.hypot(
+        pellet.position.x - player2.position.x,
+        pellet.position.y - player2.position.y
+      ) <
+      pellet.radius + player2.radius
+    ) {
+      pellet.color = "red";
+    }
+  });
+});
+
+// player.draw();
--- scripts/main.js
+++ scripts/main.js
@@ -1,65 +1,74 @@
 const { fromEvent } = rxjs;
-const { map, debounceTime, mapTo, filter} = rxjs.operators;
-const { merge, interval}= rxjs
+const { map, debounceTime, mapTo, filter } = rxjs.operators;
+const { merge, interval } = rxjs;
 
 const inputField = document.getElementById("input-field");
 const displayText = document.getElementById("display-text");
 
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
 class Packman {
-  constructor({position, velocity}) {
+  constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
     this.radius = 20;
   }
 
   draw() {
-    c.beginPath()
-    c.arc(this.position.x + this.radius, this.position.y + this.radius, this.radius, 0, Math.PI * 2)
-    c.fillStyle = 'white'
-    c.fill()
-    c.closePath()
+    c.beginPath();
+    c.arc(
+      this.position.x + this.radius,
+      this.position.y + this.radius,
+      this.radius,
+      0,
+      Math.PI * 2
+    );
+    c.fillStyle = "white";
+    c.fill();
+    c.closePath();
   }
 
-  move(){
-    this.draw()
-    this.position.x = (this.position.x + this.velocity.x)%300
-    this.position.y = (this.position.y + this.velocity.y)%300
-    console.log(this.position)
+  move() {
+    this.draw();
+    this.position.x = (this.position.x + this.velocity.x) % 300;
+    this.position.y = (this.position.y + this.velocity.y) % 300;
+    console.log(this.position);
   }
 }
 
-const player = new Packman({position: {x: 0, y: 0}, velocity: {x: 0, y: 0}})
+const player = new Packman({
+  position: { x: 0, y: 0 },
+  velocity: { x: 0, y: 0 },
+});
 
-
-const leftArrow$ = fromEvent(document, 'keydown').pipe(
-  filter(event => event.key === 'ArrowLeft'),
+const leftArrow$ = fromEvent(document, "keydown").pipe(
+  filter((event) => event.key === "ArrowLeft"),
   mapTo({ x: -10, y: 0 })
 );
-const rightArrow$ = fromEvent(document, 'keydown').pipe(
-  filter(event => event.key === 'ArrowRight'),
+const rightArrow$ = fromEvent(document, "keydown").pipe(
+  filter((event) => event.key === "ArrowRight"),
   mapTo({ x: 10, y: 0 })
 );
-const upArrow$ = fromEvent(document, 'keydown').pipe(
-  filter(event => event.key === 'ArrowUp'),
+const upArrow$ = fromEvent(document, "keydown").pipe(
+  filter((event) => event.key === "ArrowUp"),
   mapTo({ x: 0, y: -10 })
 );
-const downArrow$ = fromEvent(document, 'keydown').pipe(
-  filter(event => event.key === 'ArrowDown'),
+const downArrow$ = fromEvent(document, "keydown").pipe(
+  filter((event) => event.key === "ArrowDown"),
   mapTo({ x: 0, y: 10 })
 );
 
-
 const movepackmanP1$ = merge(leftArrow$, rightArrow$, upArrow$, downArrow$);
 
-movepackmanP1$.subscribe(distance => {
+movepackmanP1$.subscribe((distance) => {
   player.velocity = distance;
 });
- const cicle = interval(50);
+const cicle = interval(50);
 
- cicle.subscribe(() => {c.clearRect(0,0,canvas.width, canvas.height);player.move()});
+cicle.subscribe(() => {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.move();
+});
 
-
-player.draw()
+player.draw();
